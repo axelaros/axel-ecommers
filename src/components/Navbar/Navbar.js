cf101b633b76bcb696aaa3c1ac8191259486fdcd
unknown <axel.aros1@gmail.com>
2022-08-20 14:15:40 -0300

    menu-axel_ecommers

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+const Navbar =()=>{
+    return(
+<header>
+    <nav className="navbar">
+        <h1 className="titulo">Electro mania</h1>
+        <ul className="cabezera">
+            <li>
+                <a href="https://www.google.com/?hl=es "> Productos</a>
+             </li>
+            <li>
+                <a href="https://www.google.com/?hl=es "> Contacto </a>
+            </li>
+            <li>
+                <a href="https://www.google.com/?hl=es ">  Nosotros </a>
+             </li>
+
+        </ul>
+            
+    </nav>
+</header>
+
+    )
+}
+
+export default Navbar;
